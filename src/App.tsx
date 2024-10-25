738d95d362aeb53f857da3530d776114821df88a
mpawelkiewiczpsk <mpawelkiewicz@tu.kielce.pl>
2024-10-25 17:06:58 +0200

    added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import * as React from "react";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from './pages/error-page.tsx';
+import Contact from './pages/contact.tsx';
+import Login from './pages/login.tsx';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "contacts/:contactId",
+                element: <Contact />,
+            },
+        ],
+    },
+    {
+        path: "/login",
+        element: <Login />,
+
+    },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <RouterProvider router={router} />
     </>
   )
 }
---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+
+export default function Login() {
+
+    return (
+        <div id="login">
+            <h1>Logowanie</h1>
+            <a href={`/`}>Zaloguj się</a>
+        </div>
+    );
+}
+
